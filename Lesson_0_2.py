94aa4290033ef83b20cd52de5fce57fff2c8b103
AnMarMag569 <m.gordeeva63@gmail.com>
2024-06-09 21:09:48 +0200

    Add files via upload
    Выполнение задание к разделу Python введение

---
+++ Lesson_0_2.py
@@ -0,0 +1,33 @@
+# Задача - длинна слова
+a="Marina"
+print(a)
+print(len(a))
+
+# Задача - сумма и разности
+first=10
+second=25
+summa=first+second
+diff=first-second
+print(summa)
+print(diff)
+
+# Задача - среднее арифметическое
+first=15
+second=16
+third=2
+mean=(first+second+third)/3
+print(mean)
+
+# Задача - простые строчки
+first_string="Вторник"
+second_string="Понедельник"
+print((second_string)+', ' + (first_string))
+
+# Задача - сложная формула
+a=2
+b=3
+c=4
+f=(a*b)+(a*c)
+print(f)
+f= (((a*b)+(a*c))**3)/2
+print(f)
